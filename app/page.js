b446e6f426571076506b8b56943ff73a129bf95b
ANAMASGARD <chaudharygaurav2004@gmail.com>
2025-05-21 21:57:20 +0530

    Authentication has been added using the Clerk Auth

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs"
+import { User } from "@clerk/nextjs/dist/types/server"
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <UserButton />
     </div>
   )
 }
