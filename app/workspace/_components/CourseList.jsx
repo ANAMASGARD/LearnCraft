7574811c1b139b7790885133a0064dccee3d84fd
ANAMASGARD <chaudharygaurav2004@gmail.com>
2025-05-22 21:43:05 +0530

    Dashboard basic work has been completed

---
+++ app/workspace/_components/CourseList.jsx
@@ -0,0 +1,30 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+
+function CourseList() {
+
+    const [courses, setCourseList] = useState([])
+    return (
+        <div className='mt-10'>
+            <h2 className='font-bold text-3xl'>CourseList </h2>
+
+            {courses?.length == 0 ? (
+                <div className='flex p-7 items-center justify-center flex-col border-2 border-dashed border-gray-300 rounded-lg mt-10 bg-secondary' >
+                    <Image src={'/rocket.gif'} alt="Go to moon" width={100} height={100}  />
+                <h2 className='my-2 text-large font-bold '> Looks Like you haven't made any courses yet </h2>
+<Button>+Create your first course </Button>
+
+                </div>
+            ) : (
+                <div>
+                    List of Courses
+                </div>
+            )}  
+        </div>
+    )
+}
+
+export default CourseList
--- app/workspace/provider.jsx
+++ app/workspace/provider.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "./_components/AppSidebar"
 import AppHeader from './_components/AppHeader'
+import WelcomeBanner from './_components/WelcomeBanner'
 
-const WorkspaceProvider = ({children }) => {
+function WorkspaceProvider({children }) {
   return (
     <SidebarProvider>
         <AppSidebar />
          
         <div className="flex flex-col w-full h-screen">
             <AppHeader />
-                {children }</div>
+            <div className="p-10 overflow-y-auto">
+                {children }
+                </div>
+                </div>
     </SidebarProvider>
     
   )
