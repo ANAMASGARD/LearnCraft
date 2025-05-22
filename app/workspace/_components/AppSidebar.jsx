68fc985de690f4c9f8fe7ece682e89ca4418c61a
ANAMASGARD <chaudharygaurav2004@gmail.com>
2025-05-22 20:58:37 +0530

    Sidebar & Header have been added

---
+++ app/workspace/_components/AppSidebar.jsx
@@ -0,0 +1,102 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem
+
+} from "@/components/ui/sidebar"
+import { BookA, Compass, LayoutDashboard, PencilRulerIcon, UserCircle2Icon, WalletCards } from "lucide-react"
+import Link from "next/link"
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar"
+
+import Image from "next/image"
+import { usePathname } from "next/navigation"
+
+const SidebarOptions = [ {
+    title: "Dashboard",
+    icon:LayoutDashboard,
+    path:'/workspace',
+},
+{
+    title: "My Learning",
+    icon: BookA,
+    path:'/workspace/my-courses',
+},{
+    title: "Explore Courses",
+    icon:Compass,
+    path:'/workspace/explore',
+},{
+    title: "AI Tools",
+    icon: PencilRulerIcon,
+    path:'/workspace/ai-tools',
+},{
+    title: "Billing",
+    icon:WalletCards,
+    path:'/workspace/billing',
+},{
+    title: "Profile",
+    icon:UserCircle2Icon,
+    path:'/workspace/profile',
+},
+]
+
+export function AppSidebar() {
+
+    const path=usePathname();
+
+  return (
+    <Sidebar>
+      <SidebarHeader className={'p-4'} > 
+        <Image
+          src={'/logo.svg'}
+          alt="Logo"
+          width={50}
+          height={30}
+          
+        />
+      
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup >
+         <Button>
+            Create new course
+         </Button>
+
+            </SidebarGroup>
+        <SidebarGroup >
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {SidebarOptions.map((item,index ) => (
+                        <SidebarMenuItem key={index}>
+                            <SidebarMenuButton asChild className={'p-5'}>
+                                <Link href ={item.path} className={`text-[17px]
+                                ${path.includes(item.path) && 'text-primary bg-purple-50'}`
+                                    
+                                }>
+                                <item.icon className="h-7 w-7" />
+                                    <span >
+                                        {item.title}
+                                    </span>
+                                </Link>
+                                </SidebarMenuButton>
+
+                            
+                           
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+            </ SidebarGroup >
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
