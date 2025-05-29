fb644f73e599220380280cd6b59ffb93e3795ef1
ANAMASGARD <chaudharygaurav2004@gmail.com>
2025-05-29 21:48:37 +0530

    Display Course Content {Some error in the ChapterListSidebar}

--- app/course/_components/ChapterContent.jsx
+++ app/course/_components/ChapterContent.jsx
@@ -8,17 +8,36 @@ function ChapterContent({courseInfo}) {
    const courseContent = courseInfo?.courses?.courseContent;
    const {selectedChapterIndex, setSelectedChapterIndex} = useContext(SelectedChapterIndexContext);
    const videoData = courseContent?.[selectedChapterIndex]?.youtubeVideo;
+   const topics = courseContent?.[selectedChapterIndex]?.courseData?.topics;
  return (
    <div className='p-10'>
       <h2 className='font-bold text-2xl '>{courseContent?.[selectedChapterIndex]?.courseData?.chapterName} </h2>
-      <h2 className='my-2 font-bold text-lg'> Related Videos </h2>
-      <div>
-           {videoData?.map((video, index) => (
+      <h2 className='my-2 font-bold text-lg'> Related Videos 🎬 </h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+           {videoData?.map((video, index) => index < 2 && (
                <div key={index}>
                    <YouTube
-                   videoId={video?.id.videoId}/>
+                   videoId={video?.id.videoId}
+                   opts={{
+                    height: '300',
+                    width: '400',
+                    
+                   }}
+                   />
                     </div>
            ))}
+      </div>
+      <div className='mt-7'>
+           {topics?.map((topic, index) => (
+            <div key={index} className='mt-10 p-4 bg-secondary rounded-2xl mb-3'>
+              <h2 className='font-bold text-2xl text-primary'> {index+1}.  {topic?.topic}</h2>
+              <div dangerouslySetInnerHTML={{ __html: topic?.content }} 
+               style={{ lineHeight: '2.5', fontSize: '17px', color: '#333' }}>
+                
+                 </div>
+               </div>
+           ))}
+
       </div>
    </div>
  )
